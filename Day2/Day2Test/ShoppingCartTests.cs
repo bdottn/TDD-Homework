dcbbc9659f647243ba70a98830f965b75f90d11c
Echo Lee <bdottn.lee+account.git@gmail.com>
2016-03-16 14:30:10 +0800

    建立第一個單元測試
    CalculateFeeTest_第一集買1本_第二集買0本_第三集買0本_第四集買0本_第五集買0本_售價為100元

---
+++ Day2/Day2Test/ShoppingCartTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Day2.Tests
+{
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        [TestMethod]
+        public void CalculateFeeTest_第一集買1本_第二集買0本_第三集買0本_第四集買0本_第五集買0本_售價為100元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            var books =
+                new List<HarryPotter>()
+                { 
+                    new HarryPotter() { Episode = 1 }
+                };
+
+            var expected = 100;
+
+            // act
+            var actual = target.CalculatePrice(books);
+
+            // atual
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
