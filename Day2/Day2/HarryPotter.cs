5038c58d9bfbfbbf0e79135bd985562fa2b8c70a
Echo Lee <bdottn.lee+account.git@gmail.com>
2016-03-16 14:35:58 +0800

    實作通過第一個單元測試

---
+++ Day2/Day2/HarryPotter.cs
@@ -0,0 +1,15 @@
+﻿namespace Day2
+{
+    public class HarryPotter
+    {
+        public int Episode { get; set; }
+
+        public int Price
+        {
+            get
+            {
+                return 100;
+            }
+        }
+    }
+}
---
+++ Day2/Day2/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Day2
+{
+    public class ShoppingCart
+    {
+        public int CalculatePrice(ICollection<HarryPotter> books)
+        {
+            var result = 0;
+
+            foreach (var book in books)
+            {
+                result = result + book.Price;
+            }
+
+            return result;
+        }
+    }
+}
