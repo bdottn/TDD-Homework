31bcf7e07b6f22fc1c47513e02ef22b9d1c82b57
Echo Lee <bdottn.lee+account.git@gmail.com>
2016-03-16 16:08:26 +0800

    建立第六個單元測試
    CalculateFeeTest_第一集買1本_第二集買1本_第三集買1本_第四集買1本_第五集買1本_售價為375元

--- Day2/Day2Test/ShoppingCartTests.cs
+++ Day2/Day2Test/ShoppingCartTests.cs
@@ -118,5 +118,30 @@ namespace Day2.Tests
             // atual
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CalculateFeeTest_第一集買1本_第二集買1本_第三集買1本_第四集買1本_第五集買1本_售價為375元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            var books =
+                new List<HarryPotter>()
+                { 
+                    new HarryPotter() { Episode = 1 },
+                    new HarryPotter() { Episode = 2 },
+                    new HarryPotter() { Episode = 3 },
+                    new HarryPotter() { Episode = 4 },
+                    new HarryPotter() { Episode = 5 },
+                };
+
+            var expected = 375;
+
+            // act
+            var actual = target.CalculatePrice(books);
+
+            // atual
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
